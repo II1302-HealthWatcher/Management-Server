6a7f58058288ef9f95b0aa52edfd21602003c628
DarkPointer <simonqs12@gmail.com>
2021-04-20 18:39:11 +0200

    Created the project structure, xUnit tests and integrated the project with Travis-CI
    Created the health watcher management server project structure, xUnit tests and integrated Travis-CI automatic builds and tests

---
+++ ManagementServer.ConsoleApp/startup/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ManagementServer.ConsoleApp
+{
+    public class Program
+    {
+        public int dummy()
+        {
+            return 1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
