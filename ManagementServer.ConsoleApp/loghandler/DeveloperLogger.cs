53dd0c4bfd31fe400e58f9b4bcf6ec90f9cc526b
Karoline Malko <karoline.malko@gmail.com>
2021-05-07 21:22:42 +0200

    Created the exception logger interface, in addition to implementing the interfaces in the developer logger and user logger classes
    Created the exception logger interface, in addition to implementing the interfaces in the developer logger and user logger classes.

---
+++ ManagementServer.ConsoleApp/loghandler/DeveloperLogger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace ManagementServer.ConsoleApp.loghandler
+{
+    public class DeveloperLogger : IExceptionLogger
+    {
+        private string filePath;
+
+        /// <summary>
+        /// Initializes an instance of the <see cref="DeveloperLogger"/> class.
+        /// </summary>
+        public DeveloperLogger()
+        {
+            try
+            {
+                DateTime currentDate = DateTime.Now;
+                string currentDateFormatted = currentDate.ToString("yyyy-MM-dd");
+                string logFilename = $"log-{currentDateFormatted}.txt";
+                this.filePath = logFilename;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("The developer logger failed:");
+                Console.WriteLine($"{exception.GetType().Name} : {exception.Message}");
+                Console.WriteLine(exception.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// Logs an exception with its stack trace to a file on the disk.
+        /// </summary>
+        /// <param name="exceptionToLog">The exception to be logged.</param>
+        public void Log(Exception exceptionToLog)
+        {
+            try
+            {
+                DateTime currentDate = DateTime.Now;
+                string currentDateTimeFormatted = currentDate.ToString("yyyy-MM-dd hh:mm:ss");
+                string exceptionTypeAndMessage = $"{currentDateTimeFormatted} - { exceptionToLog.GetType().Name } : { exceptionToLog.Message}";
+                string exceptionStackTrace = exceptionToLog.StackTrace;
+                string logContent = exceptionTypeAndMessage + Environment.NewLine + exceptionStackTrace + Environment.NewLine + Environment.NewLine;
+                File.AppendAllText(this.filePath, logContent);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("=======================================");
+                Console.WriteLine("The developer logger failed:");
+                Console.WriteLine($"{exception.GetType().Name} : {exception.Message}");
+                Console.WriteLine(exception.StackTrace);
+                Console.WriteLine("=======================================");
+            }
+        }
+    }
+}
---
+++ ManagementServer.ConsoleApp/loghandler/IExceptionLogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ManagementServer.ConsoleApp.loghandler
+{
+    public interface IExceptionLogger
+    {
+        /// <summary>
+        /// Logs an exception message.
+        /// </summary>
+        /// <param name="exceptionToLog">The exception to be logged.</param>
+        public void Log(Exception exceptionToLog);
+    }
+}
